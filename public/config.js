d81e82693b76ae11480e15ce814367ac742ee09b
Mario Reder <mario.reder@pm.me>
2025-11-20 21:42:09 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/honeypotfinance",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/honeypot-finance-9999/",
+  "VITE_SEO_SITE_URL": "https://perp.honeypotfinance.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
